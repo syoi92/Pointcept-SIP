f7ae1edebaa2dddfa695e0389c544ad5138c162c
syoi92 <syoi0928@gmail.com>
2025-07-29 17:31:43 -0400

    WIP: sip config

---
+++ pointcept/datasets/sip.py
@@ -0,0 +1,84 @@
+"""
+S3DIS Dataset
+
+Author: Seongyong Kim (syoi0928@gmail.com)
+"""
+
+import os
+from .defaults import DefaultDataset
+from .builder import DATASETS
+
+
+@DATASETS.register_module()
+class SIPDataset(DefaultDataset):
+        VALID_ASSETS = [
+        "coord",
+        "color",
+        "normal",
+        "intensity",
+        "segment",
+        # "instance",
+    ]
+
+
+    def get_data_list(self):
+        if isinstance(self.split, str):
+            split_list = [self.split]
+        elif isinstance(self.split, Sequence):
+            split_list = self.split
+        else:
+            raise NotImplementedError
+
+        data_list = []
+        for split in split_list:
+            if os.path.isfile(os.path.join(self.data_root, split)):
+                with open(os.path.join(self.data_root, split)) as f:
+                    data_list += [
+                        os.path.join(self.data_root, data) for data in json.load(f)
+                    ]
+            else:
+                data_list += glob.glob(os.path.join(self.data_root, split, "*"))
+        return data_list
+
+    def get_data(self, idx):
+        data_path = self.data_list[idx % len(self.data_list)]
+        name = self.get_data_name(idx)
+        split = self.get_split_name(idx)
+        if self.cache:
+            cache_name = f"pointcept-{name}"
+            return shared_dict(cache_name)
+
+        data_dict = {}
+        assets = os.listdir(data_path)
+        for asset in assets:
+            if not asset.endswith(".npy"):
+                continue
+            if asset[:-4] not in self.VALID_ASSETS:
+                continue
+            data_dict[asset[:-4]] = np.load(os.path.join(data_path, asset))
+        data_dict["name"] = name
+        data_dict["split"] = split
+
+        if "coord" in data_dict.keys():
+            data_dict["coord"] = data_dict["coord"].astype(np.float32)
+
+        if "color" in data_dict.keys():
+            data_dict["color"] = data_dict["color"].astype(np.float32)
+
+        if "normal" in data_dict.keys():
+            data_dict["normal"] = data_dict["normal"].astype(np.float32)
+
+        if "segment" in data_dict.keys():
+            data_dict["segment"] = data_dict["segment"].reshape([-1]).astype(np.int32)
+        else:
+            data_dict["segment"] = (
+                np.ones(data_dict["coord"].shape[0], dtype=np.int32) * -1
+            )
+
+        if "instance" in data_dict.keys():
+            data_dict["instance"] = data_dict["instance"].reshape([-1]).astype(np.int32)
+        else:
+            data_dict["instance"] = (
+                np.ones(data_dict["coord"].shape[0], dtype=np.int32) * -1
+            )
+        return data_dict
